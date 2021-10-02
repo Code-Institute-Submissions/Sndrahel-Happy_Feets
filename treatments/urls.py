5727f7e959567254ca68e03835fa9ebfee3d7faf
Sndrahel <sandra.helander@hotmail.com>
2021-10-02 20:41:56 +0000

    Chore(Treatments-Package): Added packages details and fixed some minor spelling

--- treatments/urls.py
+++ treatments/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.all_treatments, name='treatments')
+    path('', views.all_treatments, name='treatments'),
+    path('<package_id>', views.package_detail, name='package_detail'),
 ]
