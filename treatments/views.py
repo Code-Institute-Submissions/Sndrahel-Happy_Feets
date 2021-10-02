5a1576009f271239a1c762ec414a0b5b92873492
Sndrahel <sandra.helander@hotmail.com>
2021-10-02 19:19:16 +0000

    Chore(Treatments-page): started treatments.html and added urls and views

--- treatments/views.py
+++ treatments/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Package
 
-# Create your views here.
+
+def all_treatments(request):
+    """ A view to return the treatments package """
+
+    treatments = Package.objects.all()
+
+    context = {
+        'treatments': treatments,
+    }
+
+    return render(request, 'treatments/treatments.html', context)
