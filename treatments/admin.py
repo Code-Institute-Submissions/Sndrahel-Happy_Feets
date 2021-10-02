79942f75f0e5d4decd613b36b250fc0e2a6e4902
Sndrahel <sandra.helander@hotmail.com>
2021-10-02 16:06:23 +0000

    Chore(Treatments-app): Added admin classes and image to package

--- treatments/admin.py
+++ treatments/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Package, Categories
 
-# Register your models here.
+
+class PackageAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'description',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+
+admin.site.register(Package, PackageAdmin)
+admin.site.register(Categories, CategoriesAdmin)
