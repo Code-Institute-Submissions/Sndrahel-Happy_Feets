70d3144ebd50f0d0bf2e6fb1a165b6b5febaf553
Sndrahel <sandra.helander@hotmail.com>
2021-10-02 15:48:54 +0000

    Chore(Treatments-app): Added categories and treatments json and code to models.py

--- treatments/models.py
+++ treatments/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(
+        max_length=254, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Category'
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Package(models.Model):
+    categories = models.ForeignKey(
+        "Categories", null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(
+        max_length=250, blank=True)
+    name = models.CharField(max_length=300)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(
+        max_length=2000, blank=True)
+    image = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.name
