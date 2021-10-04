fd3f15c0786fc925db24a83b5b71ed7fb5ecd262
Sndrahel <sandra.helander@hotmail.com>
2021-10-04 23:24:08 +0000

    Chore(index-core): Added testimonial snippet and fixed some styling

--- happy_feets/settings.py
+++ happy_feets/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
