11b45a81d84b3dd3d4b9da069f3fa1cc18ac0fe3
Sndrahel <sandra.helander@hotmail.com>
2021-10-10 00:12:18 +0000

    Chore(bag-core): Added Updated content processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from treatments.models import Package
 
 
 def bag_contents(request):
@@ -7,6 +9,17 @@ def bag_contents(request):
     bag_items = []
     total = 0
     package_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        package = get_object_or_404(Package, pk=item_id)
+        total += quantity * package.price
+        package_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'package': package,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
