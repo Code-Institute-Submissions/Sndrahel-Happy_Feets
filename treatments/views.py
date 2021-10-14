8ef769b312c2789ccf775655b92f8cb8c95a7297
Sndrahel <sandra.helander@hotmail.com>
2021-10-14 23:22:37 +0000

    Chore(treatments-app): Added login secure to treatments handling

--- treatments/views.py
+++ treatments/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Package, Category
@@ -35,7 +36,6 @@ def all_treatments(request):
             treatments = treatments.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-        """ View to filter packages through search bar """
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -71,8 +71,13 @@ def package_detail(request, package_id):
     return render(request, 'treatments/package_detail.html', context)
 
 
+@login_required
 def add_package(request):
     """ Add a package to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     if request.method == 'POST':
         form = PackageForm(request.POST, request.FILES)
         if form.is_valid():
@@ -92,8 +97,13 @@ def add_package(request):
     return render(request, template, context)
 
 
+@login_required
 def edit_package(request, package_id):
     """ Edit a package in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     package = get_object_or_404(Package, pk=package_id)
     if request.method == 'POST':
         form = PackageForm(request.POST, request.FILES, instance=package)
@@ -116,8 +126,13 @@ def edit_package(request, package_id):
     return render(request, template, context)
 
 
+@login_required
 def delete_package(request, package_id):
     """ Delete a package from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     package = get_object_or_404(Package, pk=package_id)
     package.delete()
     messages.success(request, 'Package deleted!')
