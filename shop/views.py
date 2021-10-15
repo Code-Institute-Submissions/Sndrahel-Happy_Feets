ee6e8c9d3dab44dbc9bec48ff573ec3293607b67
Sndrahel <sandra.helander@hotmail.com>
2021-10-15 21:37:54 +0000

    Chore(Shop-app): Added views to shop products

--- shop/views.py
+++ shop/views.py
@@ -1,14 +1,59 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+
+from django.db.models import Q
+from django.db.models.functions import Lower
+from .models import Product, Category
+
 
 
 def shop_products(request):
-    """ A view to return the products """
+    """ A view to return the shop products """
 
     shop = Product.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                shop = shop.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            shop = shop.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            shop = shop.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "Sorry, you didn't enter any search criteria!")
+                return redirect(reverse('shop'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            shop = shop.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'shop': shop,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'shop/shop.html', context)
