b482796199cecad29b68b59d954ab8c77093db61
Sndrahel <sandra.helander@hotmail.com>
2021-10-15 21:10:13 +0000

    Chore(Shop-app): Added products, views, models and templates

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from .models import Product
 
 
-def shop(request):
-    """ A view to return the classes """
+def shop_products(request):
+    """ A view to return the products """
 
-    return render(request, 'shop/shop.html')
+    shop = Product.objects.all()
+
+    context = {
+        'shop': shop,
+    }
+
+    return render(request, 'shop/shop.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual shop products """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'shop/product_detail.html', context)
