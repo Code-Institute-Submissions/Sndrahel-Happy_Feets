5f1f29b8257e80debd33347ad4f1dfe46d70cb90
Sndrahel <sandra.helander@hotmail.com>
2021-10-16 17:25:37 +0000

    test(ratings-function): working on ratings function

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -24,11 +25,33 @@ class Product(models.Model):
     name = models.CharField(max_length=300)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(
-        max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(
         max_length=2000, blank=True)
     image = models.ImageField(blank=True)
 
     def __str__(self):
         return self.name
+
+# Add Rating to Product Description. Code adapted from: https://amethyst-lynx-91ku816p.ws-eu17.gitpod.io/
+    def get_rating(self):
+        total = sum(int(review['stars']) for review in self.reviews.values())
+
+        if self.reviews.count() > 0:
+            return total / self.reviews.count()
+        else:
+            return 0
+
+
+# Product Review. Code adapted from: https://amethyst-lynx-91ku816p.ws-eu17.gitpod.io/
+class ProductReview(models.Model):
+    product = models.ForeignKey(
+        Product, related_name='reviews', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, related_name="reviews", on_delete=models.CASCADE
+    )
+
+    content = models.TextField(blank=True)
+    stars = models.IntegerField()
+
+    date_added = models.DateTimeField(auto_now_add=True)
--- shop/views.py
+++ shop/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 
-from .models import Product, Category
+from .models import Product, Category, ProductReview
 from .forms import ProductForm
 
 
@@ -64,6 +64,21 @@ def product_detail(request, product_id):
     """ A view to show individual shop products """
 
     product = get_object_or_404(Product, pk=product_id)
+
+    # Add product review (Code below adapted from: https://amethyst-lynx-91ku816p.ws-eu17.gitpod.io/)
+
+    if request.method == 'POST' and request.user.is_authenticated:
+        stars = request.POST.get('stars', 5)
+        content = request.POST.get('content', '')
+        user = request.POST.get('user', '')
+
+        review = ProductReview.objects.create(
+            product=product, user=request.user, stars=stars,
+            content=content)
+
+        return redirect('product_detail', product_id)
+
+    # Product Review code ends here
 
     context = {
         'product': product,
