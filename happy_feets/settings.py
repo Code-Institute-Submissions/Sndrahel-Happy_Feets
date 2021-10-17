2690e66ae3c7a003a4f9dbe82570ba8d00436832
Sndrahel <sandra.helander@hotmail.com>
2021-10-17 15:20:41 +0000

    Deployment commit

--- happy_feets/settings.py
+++ happy_feets/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
-from pathlib import Path
-
 import dj_database_url
+
+from pathlib import Path
 
 if os.path.isfile("env.py"):
     import env  # noqa
