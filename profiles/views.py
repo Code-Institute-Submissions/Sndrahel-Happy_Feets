6fa975c234b193b4ec394ad02b3ef47c40b82819
Sndrahel <sandra.helander@hotmail.com>
2021-10-19 05:44:44 +0000

    testing to sort out a bug with 500 error

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+
 from .models import UserProfile
 from .forms import UserProfileForm
 
@@ -9,7 +10,8 @@ from checkout.models import Order
 
 @login_required
 def profile(request):
-    """ Display the user's profile. """
+    """ Display the user's profile """
+
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
@@ -18,21 +20,21 @@ def profile(request):
             form.save()
             messages.success(request, 'Profile updated successfully')
         else:
-            messages.error(
-                request, 'Update failed. Please ensure the form is valid.')
+            messages.error(request, 'Update failed. Please ensure the form is \
+                valid.')
     else:
         form = UserProfileForm(instance=profile)
-    orders = profile.orders.all()
+
+    orders = profile.orders.all().order_by('-date')
 
     template = 'profiles/profile.html'
     context = {
         'form': form,
         'orders': orders,
-        'on_profile_page': True
+        'on_profile_page': True,
     }
 
     return render(request, template, context)
-
 
 @login_required
 def order_history(request, order_number):
